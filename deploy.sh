6ba4461344ad336f3896635603e13e41858d4e64
lordrigar <wojciechzaorski@gmail.com>
2020-09-18 07:59:45 +0200

    amend scripts

--- deploy.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-rsync -avz --delete --exclude-from=.rsyncignore -e ssh . do_nerdy:/var/www/zaorski.co.uk/html && \
-        ssh do_nerdy 'cd /var/www/zaorski.co.uk/html && ./build.sh'
-exit 1
