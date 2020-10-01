2ae6bce4613adce41607bc5a692bf105b4a57b56
lordrigar <wojciechzaorski@gmail.com>
2020-10-01 11:50:43 +0200

    mongo cluster

--- mongo/mongo_setup.sh
+++ mongo/mongo_setup.sh
@@ -21,31 +21,27 @@ docker exec $(docker ps -aqf "name=mongo1") mongo --eval '
         "host": "mongo3:27017",
         "priority": 0
       },
-    ]})
-'
+    ]});
 
-docker exec $(docker ps -aqf "name=mongo1") mongo --eval '
-admin = db.getSiblingDB("admin")
-admin.createUser(
-  {
-    user: "root",
-    pwd: "admin",
-    roles: [ { role: "root", db: "admin" } ]
-  }
-)
-'
+    admin = db.getSiblingDB("admin");
+    admin.createUser(
+      {
+        user: "root",
+        pwd: "admin",
+        roles: [ { role: "root", db: "admin" } ]
+      }
+    );
 
-docker exec $(docker ps -aqf "name=mongo1") mongo --eval '
-use admin
-db.createUser(
-  {
-    user: "user",
-    pwd: "password",
-    roles:
-    [
-      { role: "readWrite", db: "mymongo" }
-    ],
-    mechanisms: [ "SCRAM-SHA-1" ] 
-  }
-)
+    use admin;
+    db.createUser(
+    {
+      user: "user",
+      pwd: "password",
+      roles:
+      [
+        { role: "readWrite", db: "mymongo" }
+      ],
+      mechanisms: [ "SCRAM-SHA-1" ] 
+    }
+  );
 '
