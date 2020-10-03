59c13f404abae34761b5fe678374f5bfc7f12e70
lordrigar <wojciechzaorski@gmail.com>
2020-10-03 23:57:06 +0200

    update files

---
+++ build_swarm.example.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+docker swarm leave --force
+docker system prune --all --force
+docker image prune --all --force
+docker volume prune --all --force
+
+docker swarm init --advertise-addr <host>
+
+mkdir /var/www/<folder_path>/html/mongo/mongo-config-1
+mkdir /var/www/<folder_path>/html/mongo/mongo-config-2
+mkdir /var/www/<folder_path>/html/mongo/mongo-config-3
+
+mkdir /var/www/<folder_path>/html/mongo/mongo-data-1
+mkdir /var/www/<folder_path>/html/mongo/mongo-data-2
+mkdir /var/www/<folder_path>/html/mongo/mongo-data-3
+
+docker stack deploy --compose-file=docker-compose.yml myapp
+
+./mongo/mongo_setup.sh
